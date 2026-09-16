2c71fc131d05a253898ffef77e7942d7302879c5
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-16 16:30:11 +0530

    setup worker to convert pdf to text for ai

---
+++ core/tasks.py
@@ -0,0 +1,16 @@
+from django.shortcuts import get_object_or_404
+from celery import shared_task
+import fitz
+from core.models import Resume
+
+@shared_task
+def resume_pdf_to_text(resumeid):
+    resume_data=get_object_or_404(Resume, id=resumeid)
+    doc = fitz.open(resume_data.resume)
+    text=''
+    for page_num in range(len(doc)):
+        page = doc.load_page(page_num)
+        data = page.get_text()
+        text=text+data
+    return text
+    
