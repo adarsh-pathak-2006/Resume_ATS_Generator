314678942f514b159fc58817a2578e1e8ae63bb2
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-16 13:37:26 +0530

    added register api endpoint

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db.models import Q
 
-# Create your views here.
+class RegisterAPI(APIView):
+    def post(self, request):
+        serial=RegisterSerializer(data=request.data)
+        if serial.is_valid():
+            username=serial.validated_data['username']
+            email=serial.validated_data['email']
+            password=serial.validated_data['password']
+
+            if User.objects.filter(Q(username=username) | Q(email=email)).exists():
+                return Response({"message":"username or email already exists."}, status=400)
+            User.objects.create_user(username=username, email=email, password=password)
+            return Response({"message":"user successfully registered"}, status=201)
+        return Response(serial.errors, status=400)
