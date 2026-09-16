fdd4f2d918011ec494dc5bb6f7c20fc20e065937
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-16 14:28:22 +0530

    resume api done

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import ResponseDatabase, Resume
+from .serializers import ResumeSerializer, ResumeUploadSerializer
+from authentication.serializers import UserGetSerializer
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+class ResumeAPI(ListCreateAPIView):
+    serializer_class=ResumeSerializer
+
+    def get_queryset(self):
+        return Resume.objects.select_related('user').filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
