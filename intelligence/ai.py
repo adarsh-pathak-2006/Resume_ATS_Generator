1f663894fb72035998c67526cdc0167da6d7ba9b
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-16 16:00:26 +0530

    ai layer setup complete

---
+++ intelligence/ai.py
@@ -0,0 +1,11 @@
+from google import genai
+from django.conf import settings
+
+client = genai.Client(api_key=settings.GEMINI_API_KEY)
+
+async def get_response(prompt):
+    response = client.models.generate_content(
+    model='gemini-2.5-flash',
+    contents=prompt
+    )
+    return response
