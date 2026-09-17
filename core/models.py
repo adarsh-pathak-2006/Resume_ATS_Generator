7ee0088d5e4d63a4ce0d2853c9726cbf540b99d6
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-17 09:54:12 +0530

    defined null in resume model

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Resume(models.Model):
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    user=models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     resume=models.FileField(upload_to='resume-pdfs/')
     added_on=models.DateTimeField(auto_now_add=True)
 
